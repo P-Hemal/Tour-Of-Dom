406c55a072985b3a076a3f11c19eb85c0dcc1f12
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-03-17 16:44:59 +0600

    dom

--- JS/Dom.js
+++ JS/Dom.js
@@ -1,1 +1,9 @@
-console.log('hello from JS');
+const sections = document.getElementsByTagName('section');
+for(const section of sections){
+    section.style.border = '2px solid blue';
+    section.style.marginBottom = '5px';
+    section.style.backgroundColor = 'goldenrod';
+    
+    section.style.textDecoration = 'none';
+
+}
