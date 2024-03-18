581503b369c4f8ed5b0b6525a18b0a0dc5df2d09
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-03-18 22:29:09 +0600

    append

--- JS/Dom.js
+++ JS/Dom.js
@@ -1,18 +1,18 @@
 const sections = document.getElementsByTagName('section');
-for(const section of sections){
+for (const section of sections) {
     section.style.border = '2px solid blue';
     section.style.marginBottom = '5px';
     section.style.backgroundColor = 'goldenrod';
-    
-    section.style.borderRadius= '5px';
+
+    section.style.borderRadius = '5px';
 
 }
 const someLi = document.getElementsByTagName('li');
-for(const li of someLi){
+for (const li of someLi) {
     li.style.listStyle = 'none';
 }
 
 const h1s = document.getElementsByTagName('h1');
-for(const h1 of h1s){
+for (const h1 of h1s) {
     h1.style.color = 'royalBlue'
 }
---
+++ JS/append.js
@@ -0,0 +1,4 @@
+const placesList = document.getElementById('Places-list');
+const li = document.createElement('li');
+li.innerText = 'Pahartoli Bon';
+placesList.appendChild(li)
