8051ffc18563bcb451c594ae568e9ffcb446ec6b
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-03-19 16:20:26 +0600

    complete

--- JS/append.js
+++ JS/append.js
@@ -1,27 +1,43 @@
-// const placesList = document.getElementById('Places-list');
-// const li = document.createElement('li');
-// li.innerText = 'Pahartoli Bon';
-// placesList.appendChild(li)
+// No 1. Where to add
+const placesList = document.getElementById('places-list');
+// No 2. what To be added
+const li = document.createElement('li');
+li.innerText = 'Sajek';
+// No 3. add teh child
+placesList.appendChild(li)
 
-// const mainContainer = document.getElementById('main-container');
-// const sectionFood = document.createElement('section');
-// const h1 = document.createElement('h1');
-// h1.innerText = 'My food list';
-// section.appendChild(h1);
-// const ul = document.createElement('ul');
-// const li1 = document.createElement('li');
-// li.innerText = 'Kacchi';
-// ul.appendChild(li1)
-// section.appendChild(ul);
-// mainContainer.appendChild(sectionFood);
+// No 1. Where to add
+const mainContainer = document.getElementById('main-container');
 
+// No 2. what To be added
+const section = document.createElement('section');
+const h1 = document.createElement('h1');
+h1.innerText = 'My food list';
+section.appendChild(h1);
 
-// const sectionDress = document.createElement('section');
-// sectionDress.innerHTML = `
-// <h1>My Dress Collection</h1>
-// <ul>
-// <li>Pant</li>
-// </ul>
-// `
+const ul = document.createElement('ul');
+const li1 = document.createElement('li');
+li1.innerText = 'Kacchi';
+ul.appendChild(li1)
+const li2 = document.createElement('li');
+li2.innerText = 'Lacchi';
+ul.appendChild(li2)
+const li3 = document.createElement('li');
+li3.innerText = 'Biriyani';
+ul.appendChild(li3)
+section.appendChild(ul);
+mainContainer.appendChild(section);
 
-// mainContainer.appendChild(sectionDress);
+// Set inner HTML directly
+const sectionDress = document.createElement('section');
+sectionDress.innerHTML = `
+<h1>My Dress Collection</h1>
+<ul>
+<li>Pant</li>
+<li>Shirt</li>
+<li>Cap</li>
+<li>Shoe</li>
+</ul>
+`
+mainContainer.appendChild(sectionDress);
+
