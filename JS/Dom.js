9d8c7a0a5811e368abcb29f3813d6d34a13868fa
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-03-19 11:19:20 +0600

    get element by class

--- JS/Dom.js
+++ JS/Dom.js
@@ -1,18 +1,1 @@
-const sections = document.getElementsByTagName('section');
-for (const section of sections) {
-    section.style.border = '2px solid blue';
-    section.style.marginBottom = '5px';
-    section.style.backgroundColor = 'goldenrod';
-
-    section.style.borderRadius = '5px';
-
-}
-const someLi = document.getElementsByTagName('li');
-for (const li of someLi) {
-    li.style.listStyle = 'none';
-}
-
-const h1s = document.getElementsByTagName('h1');
-for (const h1 of h1s) {
-    h1.style.color = 'royalBlue'
-}
+console.log('hello form js');
--- JS/append.js
+++ JS/append.js
@@ -1,4 +1,27 @@
-const placesList = document.getElementById('Places-list');
-const li = document.createElement('li');
-li.innerText = 'Pahartoli Bon';
-placesList.appendChild(li)
+// const placesList = document.getElementById('Places-list');
+// const li = document.createElement('li');
+// li.innerText = 'Pahartoli Bon';
+// placesList.appendChild(li)
+
+// const mainContainer = document.getElementById('main-container');
+// const sectionFood = document.createElement('section');
+// const h1 = document.createElement('h1');
+// h1.innerText = 'My food list';
+// section.appendChild(h1);
+// const ul = document.createElement('ul');
+// const li1 = document.createElement('li');
+// li.innerText = 'Kacchi';
+// ul.appendChild(li1)
+// section.appendChild(ul);
+// mainContainer.appendChild(sectionFood);
+
+
+// const sectionDress = document.createElement('section');
+// sectionDress.innerHTML = `
+// <h1>My Dress Collection</h1>
+// <ul>
+// <li>Pant</li>
+// </ul>
+// `
+
+// mainContainer.appendChild(sectionDress);
